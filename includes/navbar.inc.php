4c0a457ee8291391127f1336996f80b1af6279bd
Fabbernat <h259147@stud.u-szeged.hu>
2024-01-22 15:23:43 +0100

    01.22. update

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -4,6 +4,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+/**
+    The echo href paths below intentionally don't have ../, because the only place this include file should be used is in the root. Otherwise, the paths won't work.
+ */
 echo '<nav>
     <a href="index.php">Home</a> |
     <a href="register.php">Register</a> |
