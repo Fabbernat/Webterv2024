90376d1b4e4b902bdb6b29d6cadb4544392b194f
Fabbernat <h259147@stud.u-szeged.hu>
2024-03-24 18:05:36 +0100

    03.24. 15:30 egységesítés, szépítés

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -5,7 +5,7 @@
             <li style="margin-bottom: 10px;"><a href="mailto:meowuwuka@calculus.hu">meowuwuka@calculus.hu</a></li>
             <li style="margin-bottom: 10px;"><a href="https://www.facebook.com/meowuwuka">facebook.com/meowuwuka</a></li>
             <li style="margin-bottom: 10px;"><a href="https://www.linkedin.com/in/meowuwuka">linkedin.com/meowuwuka</a></li>
-            <li style="margin-bottom: 10px;"><a href="https://www.meowuwuka.com">meowuwuka.com</a></li>
+            <li style="margin-bottom: 10px;"><a href="https://www.instagram.com/meowuwuka">instagram.com/meowuwuka</a></li>
         </ul>
         <p>&copy; 2024 - 2024 TryHard33. All rights reserved.</p>
     </section>
